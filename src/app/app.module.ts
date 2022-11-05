4aace0ad026c0e09ae6d73d1383b2162c7b88b70
MatejGalic <matey.galic99@gmail.com>
2022-11-05 21:59:11 +0100

    Added header, rearrenged stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,53 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XssComponent } from './components/xss/xss.component';
-
-import { AuthModule } from '@auth0/auth0-angular';
 import { AuthButtonComponent } from './components/auth-button/auth-button.component';
-import { VulnerabilityButtonComponent } from './components/vulnerability-button/vulnerability-button.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { HomepageComponent } from './pages/homepage/homepage.component';
+import { VulnerabilityButtonComponent } from './components/vulnerability-button/vulnerability-button.component';
+import { XssComponent } from './components/xss/xss.component';
 import { AdminOverviewComponent } from './pages/admin-overview/admin-overview.component';
-import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 import { ManagerOverviewComponent } from './pages/manager-overview/manager-overview.component';
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 import { UserOverviewComponent } from './pages/user-overview/user-overview.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, XssComponent, AuthButtonComponent, VulnerabilityButtonComponent, UserProfileComponent, HomepageComponent, AdminOverviewComponent, UnauthorizedComponent, ManagerOverviewComponent, UserOverviewComponent],
+  declarations: [
+    AppComponent,
+    XssComponent,
+    AuthButtonComponent,
+    VulnerabilityButtonComponent,
+    UserProfileComponent,
+    HomepageComponent,
+    AdminOverviewComponent,
+    UnauthorizedComponent,
+    ManagerOverviewComponent,
+    UserOverviewComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     AuthModule.forRoot({
-      domain: 'dev-0o15xqyhb5vt8cs0.us.auth0.com',
-      clientId: 'ayTCNxSGPl5z0p7AY6yKj9D4oYb3yiep',
-      cacheLocation: 'localstorage'
+      domain: environment.domain,
+      clientId: environment.clientId,
+      cacheLocation: 'localstorage',
     }),
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
